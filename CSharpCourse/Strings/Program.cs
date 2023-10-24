8e687f285151f5f5aafce356f041c6291edbe8c5
yugurlu <yusufugurlu39@outlook.com>
2023-10-24 14:08:51 +0300

    strings

--- CSharpCourse/Strings/Program.cs
+++ CSharpCourse/Strings/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Intro();
+        string sentence = "My name is Yusuf Ugurlu";
 
+        var result = sentence.Length;
+        var result2 = sentence.Clone(); // not referance copy!
+
+        bool result3 = sentence.EndsWith("u");
+        bool result4 = sentence.StartsWith("y");
+
+        var result5 = sentence.IndexOf("Yusuf");
+        var result6 = sentence.LastIndexOf("Yusuf");
+        var result7 = sentence.Insert(0, "Hello, ");
+        var result8 = sentence.Substring(3);
+        var result9 = sentence.ToLower();
+        var result10 = sentence.ToUpper();
+        var result11 = sentence.Replace(" ", "-");
+        var result12 = sentence.Remove(2, 5);
+
+        Console.WriteLine(result12);
+    }
+
+    private static void Intro()
+    {
+        string city = "ankara";
+
+        foreach (var item in city)
+            Console.WriteLine(item);
+
+        string city2 = "istabul";
+
+        Console.WriteLine(string.Format("{0} {1}", city, city2));
+    }
+}
