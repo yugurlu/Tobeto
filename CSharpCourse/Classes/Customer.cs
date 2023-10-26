dbf3bd32b56329f66504069914c1fad16844c8e4
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2023-10-26 17:29:04 +0300

    Add files via upload

---
+++ CSharpCourse/Classes/Customer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+}
+
