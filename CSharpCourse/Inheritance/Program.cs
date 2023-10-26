4452cb077ada6fdda224915490104c9512cf36f3
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2023-10-26 17:32:42 +0300

    Add files via upload

---
+++ CSharpCourse/Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+internal class Program 
+{
+	static void Main(string[] args)
+	{
+		Person[] persons = new Person[]
+		{
+			new Customer{ FirstName = "Yusuf" },
+			new Student{ FirstName = "Bahadir" },
+			new Person{ FirstName = "Berat" }
+		};
+
+		foreach (Person person in persons) 
+		{
+			Console.WriteLine(person.FirstName);
+		}
+	}
+
+	class Person 
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+	}
+
+	class Person2 {}
+
+	class	Customer : Person //only one inherit object but interface different
+	{
+		public string City { get; set; } = string.Empty;
+	}
+
+	class Student : Person
+	{
+		public string Deparmant { get; set; } = string.Empty;
+	}
+}
