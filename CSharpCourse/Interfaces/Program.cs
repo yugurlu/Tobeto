b86c97647f26ea31493db73de9e3c34035efdc91
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2023-10-26 17:35:36 +0300

    Add files via upload

---
+++ CSharpCourse/Interfaces/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PersonManager personManager = new PersonManager();
+			personManager.Add(new Customer { Id = 1, FirstName = "Yusuf", LastName = "Ugurlu", Address = "Ankara" });
+
+			Console.ReadLine();
+		}
+	}
+
+    interface IPerson
+	{
+		int Id { get; set; }
+		string FirstName { get; set; }
+		string LastName { get; set; }
+	}
+
+	class Customer : IPerson
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public string Address { get; set; }
+	}
+
+	class Student : IPerson
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public string Departmant { get; set; }
+	}
+
+	class PersonManager
+	{
+		public void Add(Customer customer) 
+		{
+			Console.WriteLine(customer.FirstName + " " + customer.LastName);
+		}
+	}
+}
