ae31ecc6ca54636530aacd07601b8418b585d08f
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2023-10-26 14:52:52 +0300

    classes

---
+++ Classes/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Classes
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
+
---
+++ Classes/ProductManager.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Classes
+{
+	public class ProductManager
+	{
+        public void Add()
+        {
+            Console.WriteLine("Product Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Product Updated!");
+        }
+    }
+}
+
